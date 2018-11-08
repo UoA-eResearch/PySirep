cd8cb7a0b15f6503e1a77f97624379d3cae812e6
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-11-09 09:20:08 +1300

    nicer error messages. fixes #1

--- shell.py
+++ shell.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
+import platform
 import clr
+
+arch, os = platform.architecture()
+if arch != "32bit":
+  print("This tool only works on 32-bit versions of Python. Sorry :(")
+  exit(1)
 
 clr.AddReference("Microsoft.Tools.Connectivity")
 from Microsoft.Tools.Connectivity import *
 
 deviceDiscoveryService = DeviceDiscoveryService()
 deviceDiscoveryService.Start()
-uniqueId = deviceDiscoveryService.DevicesDiscovered()[0].UniqueId
+devices = list(deviceDiscoveryService.DevicesDiscovered())
+if len(devices) == 0:
+  print("No hololens found - is it attached via microUSB?")
+  exit(1)
+
+uniqueId = devices[0].UniqueId
 print(uniqueId)
