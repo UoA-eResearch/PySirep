da7ac78bf3448141dd7be5df98b5399d151fec43
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-11-09 16:34:01 +1300

    refactor command handling to function so that behaviour is consistent. try route commands through cmd

--- shell.py
+++ shell.py
@@ -41,58 +41,60 @@ for arg in deviceUpdateUtilArgs:
   resp = rd.RunCommand("C:\\Windows\\System32\\DeviceUpdateUtil.exe", arg)
   print("{}={}".format(arg, resp))
 
+def handle_command(input):
+  bits = input.split()
+  command = bits[0]
+  if len(bits) > 1:
+    args = " ".join(bits[1:])
+  else:
+    args = ""
+
+  if command == "reboottouefi":
+    resp = rd.RunCommand("C:\\Windows\\System32\\DeviceUpdateUtil.exe", command)
+    print(resp)
+    rd.Disconnect()
+    rd.Connect()
+    print("Reconnected")
+  elif command == "disconnect" or command == "dc":
+    rd.Disconnect()
+    print("Disconnected")
+  elif command == "connect" or command == "c":
+    rd.Connect()
+    print("Connected")
+  elif command == "getinstalledpackages":
+    resp = rd.RunCommand("C:\\Windows\\System32\\DeviceUpdateUtil.exe", command)
+    resp = resp.replace(";", "\n")
+    print(resp)
+  elif command == "get":
+    rd.GetFile(args, ".")
+    print("Got!")
+  elif command == "put":
+    remote = bits[1]
+    local = bits[2]
+    rd.PutFile(remote, local)
+    print("Put!")
+  elif command == "ping":
+    if rd.Ping():
+      print("Pong!")
+    else:
+      print("Thunk")
+  elif command == "quit" or command == "q" or command == "exit":
+    print("Bye!")
+    rd.Disconnect()
+    exit(0)
+  else:
+    resp = rd.RunCommand("C:\\Windows\\System32\\cmd.exe", "/C " + input)
+    print(resp)
+
 if len(sys.argv) > 1:
   input = ' '.join(sys.argv[1:])
-  resp = rd.RunCommand(input)
-  print(resp)
+  handle_command(input)
 else:
   #REPL
   while True:
     print("Type a command to run on the remote device")
     input = raw_input()
     try:
-      bits = input.split()
-      command = bits[0]
-      if len(bits) > 1:
-        args = " ".join(bits[1:])
-      else:
-        args = ""
-
-      if command == "reboottouefi":
-        resp = rd.RunCommand("C:\\Windows\\System32\\DeviceUpdateUtil.exe", command)
-        print(resp)
-        rd.Disconnect()
-        rd.Connect()
-        print("Reconnected")
-      elif command == "disconnect" or command == "dc":
-        rd.Disconnect()
-        print("Disconnected")
-      elif command == "connect" or command == "c":
-        rd.Connect()
-        print("Connected")
-      elif command == "getinstalledpackages":
-        resp = rd.RunCommand("C:\\Windows\\System32\\DeviceUpdateUtil.exe", command)
-        resp = resp.replace(";", "\n")
-        print(resp)
-      elif command == "get":
-        rd.GetFile(args, ".")
-        print("Got!")
-      elif command == "put":
-        remote = bits[1]
-        local = bits[2]
-        rd.PutFile(remote, local)
-        print("Put!")
-      elif command == "ping":
-        if rd.Ping():
-          print("Pong!")
-        else:
-          print("Thunk")
-      elif command == "quit" or command == "q" or command == "exit":
-        print("Bye!")
-        rd.Disconnect()
-        exit(0)
-      else:
-        resp = rd.RunCommand(command, args)
-        print(resp)
+      handle_command(input)
     except Exception as e:
       print(e)
